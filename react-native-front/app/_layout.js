78a97de6bc854100a529ebace9a1fe997c931de0
Igor Yastrebov <i.hawk@yandex.ru>
2023-12-15 11:48:46 +0300

    Started work on react native front

---
+++ react-native-front/app/_layout.js
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router';
+import { Text } from 'react-native';
+
+const Layout = () => {
+    return <>
+        <Text>TOP</Text>
+        <Stack />
+
+    </> 
+}
+
+export default Layout
